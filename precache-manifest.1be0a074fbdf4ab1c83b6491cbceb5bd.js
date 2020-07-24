ef3c499390e07956f14d86fcf4989a026af6b7f5
Felipe Silva Araujo <felipe@Felipes-iMac.local>
2020-07-23 23:20:40 -0300

    Updates

---
+++ precache-manifest.1be0a074fbdf4ab1c83b6491cbceb5bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f4787debbfff6b1d654c0aee99d8f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30e53cb77a8c366cfe8a",
+    "url": "/static/css/main.03fc4292.chunk.css"
+  },
+  {
+    "revision": "534e3b1e294254aae226",
+    "url": "/static/js/2.98c0b94d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.98c0b94d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30e53cb77a8c366cfe8a",
+    "url": "/static/js/main.b1608216.chunk.js"
+  },
+  {
+    "revision": "d756beab51522a119f69",
+    "url": "/static/js/runtime-main.9e15059f.js"
+  }
+]);
